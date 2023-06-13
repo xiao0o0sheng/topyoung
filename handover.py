4f3d4fc30a4e2ddeb745bab504fb25f7233ea843
xiao0o0sheng <117814316+xiao0o0sheng@users.noreply.github.com>
2023-06-13 12:02:02 +0800

    Add files via upload

---
+++ handover.py
@@ -0,0 +1,79 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2/13/2023 13:56
+# @Author : Xiaosheng Jin
+# @Email : xiaosheng.jin@ericsson.com
+# @File : ft.py
+# @Software: PyCharm
+
+import time
+
+from ccnbase import Ccn
+
+
+def changCCN(valuelist, p=0, q=0):
+    for i in valuelist[0]:
+        valuelist[0][i] += p
+
+    for i in valuelist[1]:
+        valuelist[1][i] += q
+
+    return valuelist
+
+
+fdd_1_1 = {5: 35, 14: 35}
+fdd_1_2 = {21: 35, 30: 35}
+fdd_2 = {37: 35, 46: 35}
+tdd_1 = {5: 28, 14: 21, 23: 22, 32: 30}
+tdd_2 = {37: 27, 46: 31, 55: 28, 64: 30}
+
+host1 = '10.164.147.74'
+host2 = '10.164.147.75'
+
+
+def mobilityHO(source, target):
+    ccn1 = Ccn(host1)
+    ccn2 = Ccn(host2)
+    def setv(valuelist):
+        if valuelist[0] != {}:
+            ccn1.set_values(valuelist[0])
+        if valuelist[1] != {}:
+            ccn2.set_values(valuelist[1])
+
+    setv(source)
+    setv(changCCN(target, 30, 30))
+
+    time.sleep(5)
+
+    print('\nUE start to handover from source to target\n')
+
+    for i in range(6):
+        print('\nThis is the %d cycle!\n' % (i + 1))
+        setv(changCCN(source, 5, 5))
+        setv(changCCN(target, -5, -5))
+
+        time.sleep(1)
+
+    print('\n' + '=' * 20 + '  wait 60s  ' + '=' * 20 + '\n')
+
+    time.sleep(30)
+
+    print('\nUE start to handover from target to source\n')
+
+    for i in range(6):
+        print('\nThis is the %d cycle!\n' % (i + 1))
+        setv(changCCN(source, -5, -5))
+        setv(changCCN(target, 5, 5))
+
+        time.sleep(1)
+
+    print('\nHandover success...\n')
+
+
+
+    del ccn1
+    del ccn2
+
+
+if __name__ == '__main__':
+    mobilityHO([{}, tdd_2], [{}, tdd_1])
---
+++ linkcurve.py
@@ -0,0 +1,55 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 4/7/2023 15:54
+# @Author : Xiaosheng Jin
+# @Email : xiaosheng.jin@ericsson.com
+# @File : linkcurve.py
+# @Software: PyCharm
+
+import time
+
+from ccnbase import Ccn
+
+
+def changCCN(valuelist, p=0):
+    for i in valuelist:
+        valuelist[i] += p
+    return valuelist
+
+
+fdd_1_1 = {5: 35, 14: 35}
+fdd_1_2 = {21: 35, 30: 35}
+fdd_2 = {37: 35, 46: 35}
+tdd_1 = {5: 28, 14: 21, 23: 22, 32: 30}
+tdd_2 = {37: 28, 46: 21, 55: 22, 64: 30}
+
+host1 = '10.164.147.74'
+host2 = '10.164.147.75'
+
+
+def link_curve(fddccn, tddccn, t, v1, v2):
+    ccn1 = Ccn(host1)
+    ccn2 = Ccn(host2)
+
+    def setv(fddccn, tddccn):
+        if fddccn != {}:
+            ccn1.set_values(fddccn)
+        if tddccn != {}:
+            ccn2.set_values(tddccn)
+
+    setv(fddccn, tddccn)
+    time.sleep(5)
+
+    print('\n start to test link curve \n')
+
+    for i in range(t):
+        print('\nThis is the %d cycle!\n' % (i + 1))
+        setv(changCCN(fddccn, v1), changCCN(tddccn, v2))
+        time.sleep(5)
+
+    del ccn1
+    del ccn2
+
+
+if __name__ == '__main__':
+    link_curve(fdd_1_1, tdd_1, 18, 3, 3)
